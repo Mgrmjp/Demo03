7df199ce148695927a3669e92e96755855e624b9
K2038 <K2038@student.jamk.fi>
2017-01-26 15:06:12 +0200

    Heater done!

--- Car/Program.cs
+++ Car/Program.cs
@@ -10,6 +10,28 @@ namespace Car
     {
         static void Main(string[] args)
         {
+            // Create a new instance from Car class
+            Car car = new Car();
+            car.PrintData();
+            car.Model = "Datsun 100A";
+            car.Color = "Yellow";
+            car.Engine = 0.5;
+            car.Speed = 10;
+            car.FuzzyDices = true;
+            car.DoorCount = 2;
+            car.Accelerate();
+            car.PrintData();
+            car.Brake(7);
+            Car.SomeProperty = 56;
+            car.PrintData();
+
+            // create another car object
+            string model = "Speedster";
+
+            Car nascar = new Car(model);
+            nascar.PrintData();
+            nascar.Color = "Black";
+            Console.WriteLine("Color is " + nascar.Color);
         }
     }
 }
