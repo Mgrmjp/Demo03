5d1166e5bc1af40013a4e7bbdc78e50342c515de
K2038 <K2038@student.jamk.fi>
2017-01-26 15:31:11 +0200

    Washer ready.

---
+++ WasherApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasherApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Washer washer = new Washer();
+            washer.IsRunning = true;
+            washer.Temperature = 45;
+            washer.RunTime = 60;
+            int mode = 0;
+
+            Console.WriteLine("Water temperature is " + washer.Temperature);
+            Console.WriteLine("The washer will run for " + washer.RunTime);
+            mode++;
+
+            if (mode == 1) washer.Wash();
+
+            else if (mode == 0) washer.Sling();
+
+
+        }
+    }
+}
---
+++ WasherApplication/Washer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasherApplication
+{
+    class Washer
+    {
+        public bool IsRunning { get; set; }
+        public int RunTime { get; set; }
+        public int Temperature { get; set; }
+
+        public void Wash()
+        {
+            Console.WriteLine("\nJag är washing the vaatteet.\n");
+        }
+
+        public void Sling()
+        {
+            Console.WriteLine("Jag är slinging the vaatteet.\n");
+        }
+
+
+    }
+}
